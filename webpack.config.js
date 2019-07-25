316ef6e964f55bf1c3e56afa1582a6f044d55ab8
doubleface <christophe@cozycloud.cc>
2019-07-25 12:59:18 +0200

    chore: upgrade webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const fs = require('fs')
 const SvgoInstance = require('svgo')
 
 const entry = require('./package.json').main
 
-const svgo = new SvgoInstance()
+const readManifest = () =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, './manifest.konnector')))
+
+const svgo = new SvgoInstance({
+  plugins: [
+    {
+      inlineStyles: { onlyMatchedOnce: false }
+    }
+  ]
+})
 
 let iconName
 try {
@@ -16,8 +26,6 @@ try {
   // console.error(`Unable to read the icon path from manifest: ${e}`)
 }
 const appIconRX = iconName && new RegExp(`[^/]*/${iconName}`)
-
-const base64Image = new Buffer(fs.readFileSync('img.jpg')).toString('base64')
 
 module.exports = {
   entry,
@@ -35,24 +43,17 @@ module.exports = {
       { from: 'assets', transform: optimizeSVGIcon },
       { from: '.travis.yml' },
       { from: 'LICENSE' }
-    ])
+    ]),
+    new webpack.DefinePlugin({
+      __WEBPACK_PROVIDED_MANIFEST__: JSON.stringify(readManifest())
+    })
   ],
   module: {
     // to ignore the warnings like :
     // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
     // Critical dependency: the request of a dependency is an expression
     // Since we cannot change this dependency. I think it won't hide more important messages
-    exprContextCritical: false,
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'string-replace-loader',
-        options: {
-          search: "fs.readFileSync('img.jpg')",
-          replace: "Buffer.from('" + base64Image + "', 'base64')"
-        }
-      }
-    ]
+    exprContextCritical: false
   }
 }
 
